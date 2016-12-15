1081b98d3a677b59a8bdb216198236c11b1e272b
redd <danielonodje@gmail.com>
2016-12-15 12:34:12 +0100

    fix bithound issues in server.js

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 import express from 'express';
-import bunyan from 'bunyan';
 import createUserRoutes from './server/users/router';
 import createDocumentRoutes from './server/documents/router';
 
 
-const logger = bunyan.createLogger({ name: 'dms-app' });
 const app = express();
 const router = express.Router();
-const PORT = process.env.PORT || 3000;
 
 createUserRoutes(router);
 createDocumentRoutes(router);
